757ae81d2b5d1d8a8db2b6886fbf3fb6207b50cc
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-10-09 00:33:28 +0530

    fixed:responsive issues are fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,13 +6,13 @@ import profileImg from "../assets/profile-2.jpg";
 function Home() {
   return (
     <div className="h-full relative">
-      <div className="h-14 w-14 rounded-tr-md rounded-br-md bg-white text-slate-600 fixed left-0 top-1/2 -translate-y-1/2 flex justify-center items-center">
+      <div className="h-14 w-14 rounded-tr-md rounded-br-md bg-white text-slate-600 fixed left-0 top-1/3 md:top-1/2 -translate-y-1/2 flex justify-center items-center z-10">
         <MdSettings size={34} className="animate-spin " />
       </div>
-      <div className="bg-primary h-full w-full clip-custom absolute top-0 left-0 -z-10"></div>
-      <div className="flex h-full ">
-        <div className="p-20  h-full w-1/2">
-          <div className="w-4/5 h-full rounded-md shadow-lg shadow-black bg-amber-100">
+      <div className="flex h-full flex-col lg:flex-row ">
+        <div className="bg-primary h-full w-full clip-custom absolute top-0 left-0 -z-10"></div>
+        <div className="p-20  md:h-full lg:w-1/2">
+          <div className="lg:w-4/5 h-full rounded-md shadow-lg shadow-black bg-amber-100">
             <img
               src={profileImg}
               alt="profile"
@@ -20,15 +20,15 @@ function Home() {
             />
           </div>
         </div>
-        <div className="w-1/2">
+        <div className="lg:w-1/2 p-10 lg:p-0 bg-black lg:bg-transparent">
           <div className="h-full w-4/5  flex flex-col justify-center  space-y-10">
-            <h1 className="text-5xl text-primary font-bold uppercase relative leading-snug">
+            <h1 className="text-3xl md:text-5xl text-primary font-bold uppercase relative leading-[3rem] md:leading-[4rem]">
               <span className="absolute -left-8 top-6 h-1 w-4 rounded bg-primary"></span>
               I'm Sivamani N.
               <br />
               <span className="text-white">Full Stack Developer</span>
             </h1>
-            <p className="text-[17px] tracking-wide leading-10 ">
+            <p className="text-[17px] tracking-wide leading-8 md:leading-10 ">
               I am a full stack developer with 3 years of experience,
               specializing in React, Node.js, Express, and MySQL. I’m passionate
               about building dynamic, user-friendly applications and solving
