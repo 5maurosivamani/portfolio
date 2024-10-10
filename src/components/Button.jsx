a31363b748ae1c9cbdda7dfac8147956afffb402
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-10-10 11:01:46 +0530

    revert: change button size back to normal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,14 +4,16 @@ function Button({ title, classes = "", icon: Icon, ...props }) {
   return (
     <div className="flex">
       <button
-        className={`group h-11 md:h-12  flex items-center justify-between rounded-full border border-light-primary text-dark-primary hover:text-white  dark:text-white relative ${classes}`}
+        className={`group h-12  flex items-center justify-between rounded-full border border-light-primary text-dark-primary hover:text-white  dark:text-white relative ${classes}`}
         {...props}
       >
-        <span className="font-bold uppercase px-5 text-nowrap z-[1]">{title}</span>
-        <div className="h-11 w-10 md:h-12 md:w-12 flex justify-center items-center rounded-full bg-light-primary  text-white z-[1]">
+        <span className="font-bold uppercase px-5 text-nowrap z-[1]">
+          {title}
+        </span>
+        <div className="h-12 w-12 flex justify-center items-center rounded-full bg-light-primary  text-white z-[1]">
           <Icon size={24} />
         </div>
-        <div className="h-full absolute top-0 right-0 w-11 md:w-12 group-hover:w-full transition-[width] duration-400 bg-light-primary  rounded-full"></div>
+        <div className="h-full absolute top-0 right-0 w-12 group-hover:w-full transition-[width] duration-400 bg-light-primary  rounded-full"></div>
       </button>
     </div>
   );
