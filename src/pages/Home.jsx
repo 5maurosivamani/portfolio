0622b1bd6ed20a9d5ec455c44d9f5267538dcb75
Sivamani <106467109+5maurosivamani@users.noreply.github.com>
2024-10-10 10:55:17 +0530

    fix:add the spacing around profile image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
       <div className="flex h-full flex-col lg:flex-row">
         <div className="bg-light-primary  h-full w-full clip-custom absolute top-0 left-0 -z-[1]"></div>
         <div className="bg-white dark:bg-dark-primary h-full w-full absolute top-0 left-0 -z-[2]"></div>
-        <div className="md:h-full lg:w-1/2 ">
+        <div className="md:h-full lg:w-1/2 my-10 lg:my-0">
           <div className="lg:w-4/5 h-full rounded-md  flex justify-center items-center">
             <div className="w-3/4 sm:w-2/4 md:w-2/5 lg:w-3/4 relative">
               <img
